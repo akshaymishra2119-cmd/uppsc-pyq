c7209b8defba5ce8419c7288b82d9a0aa79585c5
akshaymishra2119-cmd <akshay.mishra2119@gmail.com>
2026-06-19 16:39:32 +0530

    feat: latest updates

--- push-update.bat
+++ push-update.bat
@@ -4,7 +4,7 @@ git remote remove origin 2>nul
 git remote add origin https://github.com/akshaymishra2119-cmd/uppsc-pyq.git
 git add .
 git commit -m "feat: latest updates" 2>nul
-git push -u origin main
+git push -u origin main --force
 echo.
 echo Done! Railway will auto-redeploy in ~1 minute.
 pause
