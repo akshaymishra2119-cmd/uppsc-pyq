910e054014f8ec67fb652845ada2d0ae4be3bc7e
Akshay <data.work.official2026@gmail.com>
2026-06-25 13:04:05 +0530

    fix: amber MCQ color in CA + PYQ subject sidebar + mock modal redesign + undefined relevance fix

--- git_push.bat
+++ git_push.bat
@@ -2,6 +2,6 @@
 cd /d D:\uppsc_pyq
 del /f ".git\index.lock" 2>nul
 git add -A
-git commit -m "feat: mobile-responsive CA tab + 31-day progress timeline + auth flow + daily quiz tracking"
+git commit -m "fix: amber MCQ color in CA + PYQ subject sidebar + mock modal redesign + undefined relevance fix"
 git push
 pause
