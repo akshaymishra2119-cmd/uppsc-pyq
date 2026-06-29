c7af0fdeebc433acdda7245ef10fdbbcdf00d0c8
Akshay <data.work.official2026@gmail.com>
2026-06-29 16:39:24 +0530

    feat: portal update 2026-06-29

--- git_push.bat
+++ git_push.bat
@@ -5,5 +5,5 @@ git add -A
 for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set dt=%%I
 set TODAY=%dt:~0,4%-%dt:~4,2%-%dt:~6,2%
 git commit -m "feat: portal update %TODAY%"
-git push
+git push origin main
 pause
